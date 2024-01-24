329ffdec2e4ff6ea5944eecabcf2f76c00c73f38
Herminio Torres <herminiocesar@gmail.com>
2024-01-24 14:08:27 -0300

    update the shared layout
    add a new font.
    
    ```sh
    bun add @fontsource/cabin
    ```
    
    - https://www.npmjs.com/package/@fontsource/cabin
    - https://tailwindcss.com/docs/font-family#customizing-the-default-font
    
    add the `max-sm`.
    - https://tailwindcss.com/docs/responsive-design#targeting-a-breakpoint-range
    
    to center a container, use the `mx-auto` utility.
    - https://tailwindcss.com/docs/container#using-the-container
    
    setting the minimum height of an element.
    - https://tailwindcss.com/docs/min-height#setting-the-minimum-height
    
    using grid system.
    - https://css-tricks.com/introduction-fr-css-unit/
    - https://css-tricks.com/almanac/properties/g/grid-auto-rows/#aa-auto
    
    add the `Max-Width`
    utilities for setting the maximum width of an element.
    - https://tailwindcss.com/docs/max-width

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,16 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: [
+    './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Cabin', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
