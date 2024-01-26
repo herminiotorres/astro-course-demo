0ba7770eb94ffe69de6535859127d0d9044abd1c
Herminio Torres <herminiocesar@gmail.com>
2024-01-25 23:38:08 -0300

    Configuring Content Collections with Zod
    install date-fns:
    ```sh
    bun install date-fns
    ```
    
    Reference:
    - https://docs.astro.build/en/guides/content-collections/#defining-datatypes-with-zod
    - https://zod.dev/
    - https://date-fns.org/

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,7 +1,17 @@
 // 1. Import utilities from `astro:content`
-import { defineCollection } from 'astro:content';
+import { defineCollection, z } from 'astro:content';
+import { format } from 'date-fns';
 // 2. Define your collection(s)
-const postsCollection = defineCollection({ /* ... */ });
+const postsCollection = defineCollection({
+  schema: z.object({
+    author: z.string(),
+    categories: z.array(z.string()),
+    date: z.string().transform((str: string) => format(new Date(str), 'MMMM dd, yyyy')),
+    feature: z.boolean().default(false),
+    image: z.string(),
+    title: z.string()
+  })
+});
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
 export const collections = {
